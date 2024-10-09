94f7dedc71ffaa29cb1e57b583d8e2d9c966bc60
Hugo Albert <hugoalberthlu@gmail.com>
2024-10-09 18:05:24 +0200

    first version coded

--- vit.py
+++ vit.py
@@ -1,5 +1,6 @@
 import torch 
 from torch import nn
+import math
 
 #training parameters
 B = 32 #batch size
@@ -53,3 +54,142 @@ class Embedding(nn.Module):
         print("z0:", z0.shape)
         z0 = self.dropout(z0) #dropout
         return z0
+
+
+
+# Single Head Attention
+
+class Single_Head_Attention(nn.Module):
+    def __init__(self):
+        super(Single_Head_Attention, self).__init__()
+
+        self.U_qkv = nn.Linear(D, 3*Dh)
+        self.softmax = nn.Softmax(dim = -1)
+
+    def forward(self, z):
+        print("z:", z.shape)
+        qkv = self.U_qkv(z)
+        print("qkv:", qkv.shape)
+        q = qkv[:,:,:Dh]
+        print("q:", q.shape)
+        k = qkv[:,:,Dh:2*Dh]
+        print("k:", k.shape)
+        v = qkv[:,:,2*Dh:]
+        print("v:", v.shape)
+        qkTbysqrtDh = torch.matmul(q, k.transpose(-2, -2))/math.sqrt(Dh)
+        print("qkTbysqrtDh:", qkTbysqrtDh.shape)
+        A = self.softmax(qkTbysqrtDh)
+        print("A:", A.shape)
+        SAz = torch.matmul(A, v)
+        print("SAz:", SAz)
+
+        return SAz
+    
+# Multi Head Self Attention
+
+class Multi_Head_Self_Attention(nn.Module):
+    def __init__(self):
+        super(Multi_Head_Self_Attention, self).__init__()
+
+        self.heads = nn.ModuleList([Single_Head_Attention() for _ in range(k)])
+        self.U_msa = nn.Linear(D, D)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, z):
+        print("#####")
+        print("z:", z.shape)
+        ConSAz = torch.cat([head(z) for head in self.heads], dim = -1)
+        print("ConSA(z):", ConSAz.shape)
+        msaz = self.U_msa(z)
+        print("MSA(z):", msaz.shape)
+        msaz = self.dropout(msaz)
+
+        return msaz
+    
+
+# MLP
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+
+        self.U_mlp = nn.Linear(D, mlp_size)
+        self.gelu = nn.GELU()
+        self.U_mlp2 = nn.Linear(mlp_size, D)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, z):
+        print("###MLP###")
+        print("z:", z.shape)
+        z = self.U_mlp(z)
+        print("mlp(z):", z.shape)
+        z = self.gelu(z)
+        z = self.dropout(z)
+        z = self.U_mlp2(z)
+        print("mlp2(gelu(mlp(z))):", z.shape)
+        z = self.gelu(z)
+        z = self.dropout(z)
+
+        return z
+    
+
+# Transformer Block
+
+class Transformer_Block(nn.Module):
+    def __init__(self):
+        super(Transformer_Block, self).__init__()
+
+        self.layernorm_1 = nn.LayerNorm(D)
+        self.msa = Multi_Head_Self_Attention()
+        self.layernorm_2 = nn.LayerNorm(D)
+        self.mlp = MLP()
+
+    def forward(self, z):
+        print("###Transformer Block###")
+        print("z:", z.shape)
+        z1 = self.layernorm_1(z)
+        print("layernorm_1:", z1.shape)
+        z1 = self.msa(z1)
+        print("msa(layernorm_1(z)):", z1.shape)
+        z2 = z + z1
+        print("z + msa(layernorm_1(z)):", z2.shape)
+        z3 = self.layernorm_2(z2)
+        print("layernorm_2(z + msa(layernorm_1(z))):", z3.shape)
+        z3 = self.mlp(z3)
+        print("mlp(layernorm_2(z + msa(layernorm_1(z)))):", z3.shape)
+        z4 = z2 + z3
+        print("z2 + mlp(layernorm_2(z + msa(layernorm_1(z)))):", z4.shape)
+
+        return z4
+    
+
+
+# ViT
+
+class ViT(nn.Module):
+    def __init__(self):
+        super(ViT, self).__init__()
+
+        self.embedding = Embedding()
+        self.transformer_encoder = nn.ModuleList([Transformer_Block() for _ in range(L)])
+        self.layernorm = nn.LayerNorm(D)
+        self.U_mlp = nn.Linear(D, n_classes)
+
+    def forward(self, x):
+        print("###ViT###")
+        print("input image:", x.shape)
+        z = self.embedding(x)
+        print("z:", z.shape)
+        for block in self.transformer_encoder:
+            z = block(z)
+        print("z:", z.shape)
+        z = self.layernorm(z)
+        print("layernorm(z):", z.shape)
+        z = z[:,0,:]
+        print("z:", z.shape)
+        z = self.U_mlp(z)
+        print("mlp(layernorm(z)):", z.shape)
+
+        return z
+
+
